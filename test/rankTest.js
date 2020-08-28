891603231571796c74b05c5e4c779e40f3ffda43
vicky <jiajun_work@126.com>
2020-08-28 12:57:52 +0800

    [refactor homework][vicky] test: add test should return 7 when calculate voyageRisk given 4 < voyage length < 8 and voyage zone in specific zones.

--- test/rankTest.js
+++ test/rankTest.js
@@ -445,3 +445,14 @@ rankTest('should return 3 when calculate voyageRisk given 4 < voyage length < 8 
   //then
   t.is(3, result)
 });
+rankTest('should return 7 when calculate voyageRisk given 4 < voyage length < 8 and voyage zone in specific zones', t => {
+  //given
+  const voyage = {
+    zone: 'china',
+    length: 5,
+  };
+  //when
+  const result = voyageRisk(voyage);
+  //then
+  t.is(7, result)
+});
