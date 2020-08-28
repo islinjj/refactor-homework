3dd866e0a765261fbcb094cb4eb454168060d027
vicky <jiajun_work@126.com>
2020-08-28 11:59:43 +0800

    [refactor homework][vicky] test: add test should return 3 when voyageProfitFactor given voyage has china and empty history.

--- test/rankTest.js
+++ test/rankTest.js
@@ -1,5 +1,5 @@
 const rankTest = require('ava');
-const { rating,voyageProfitFactor,voyageRisk,captainHistoryRisk } = require('../src/rank');
+const { rating, voyageProfitFactor, voyageRisk, captainHistoryRisk } = require('../src/rank');
 
 rankTest('should return 2 when voyageProfitFactor given empty history and empty voyage', t => {
   //given
@@ -10,7 +10,7 @@ rankTest('should return 2 when voyageProfitFactor given empty history and empty 
   //then
   t.is(2, result);
 });
-rankTest('should return 2 when voyageProfitFactor given empty voyage and history has china',t => {
+rankTest('should return 2 when voyageProfitFactor given empty voyage and history has china', t => {
   //given
   let voyage = {};
   let history = [
@@ -23,4 +23,16 @@ rankTest('should return 2 when voyageProfitFactor given empty voyage and history
   let result = voyageProfitFactor(voyage, history);
   //then
   t.is(2, result);
+});
+rankTest('should return 3 when voyageProfitFactor given voyage has china and empty history', t => {
+  //given
+  let voyage = {
+    zone: 'china',
+    length: 10,
+  };
+  let history = [];
+  //when
+  let result = voyageProfitFactor(voyage, history);
+  //then
+  t.is(3, result);
 })
