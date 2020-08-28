7989ac2e7f4f5b3c41cf088347c80c016e644cdc
Vicky Lin <jiajun_work@126.com>
2020-08-28 20:03:22 +0800

    Revert "[refactor homework][vicky] refactor: remove unused variable."
    This reverts commit 6a8305c899707a46ec2a4153907ad83bdb5abfab.

--- src/rank.js
+++ src/rank.js
@@ -78,5 +78,25 @@ module.exports = {
   rating,voyageProfitFactor,voyageRisk,captainHistoryRisk
 };
 
+const voyage = {
+  zone: 'west-indies',
+  length: 10,
+};
+const history = [
+  {
+    zone: 'east-indies',
+    profit: 5,
+  },{
+    zone: 'west-indies',
+    profit: 15,
+  },{
+    zone: 'china',
+    profit: -2,
+  },
+  {
+    zone: 'west-africa',
+    profit: 7,
+  },
+];
 const myRating = rating(voyage, history);
 console.log(`myRating: ${myRating}`);
