0698e30c9888ce723aaa95bee0a8fdbf047524b4
Vicky Lin <jiajun_work@126.com>
2020-08-28 23:14:09 +0800

    [refactor homework][vicky] refactor: extract calculateOutstanding method.

--- src/print.js
+++ src/print.js
@@ -1,22 +1,21 @@
 function printOwing(invoice) {
-  let outstanding = 0;
   let result = `***********************\n**** Customer Owes ****\n***********************\n`
-
-  // calculate outstanding
-  for (const o of invoice.borderSpacing) {
-    outstanding += o.amount;
-  }
-
-  // record due date
+  let outstanding = calculateOutstanding(invoice);
   recordDueDate(invoice);
-
-  // print details
   return getResult(result, invoice, outstanding);
 }
 
 module.exports = {
   printOwing
 }
+function calculateOutstanding(invoice) {
+  let outstanding = 0;
+  for (const o of invoice.borderSpacing) {
+    outstanding += o.amount;
+  }
+  return outstanding;
+}
+
 function recordDueDate(invoice) {
   const today = new Date();
   invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
