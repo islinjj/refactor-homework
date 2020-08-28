bb2ee233e9db51a8d2f0b45bfaae861baa76f387
Vicky Lin <jiajun_work@126.com>
2020-08-28 22:23:46 +0800

    [refactor homework][vicky] test: add should should return 2 when deliveryDate given rush is true and ny.

---
+++ test/deliveryTest.js
@@ -0,0 +1,34 @@
+const deliveryTest = require('ava');
+const { deliveryDate } = require('../src/delivery');
+
+deliveryTest('should return 2 when deliveryDate given rush is true and ma', t => {
+    //given
+    let isRush = true;
+    let anOrder = {
+        deliveryState: 'MA',
+        placedOn: {
+            plusDays: (x) => {
+                return x
+            }
+        }
+    }
+    //when
+    const result = deliveryDate(anOrder, isRush);
+    //then
+    t.is(result, 2);
+});
+deliveryTest('should return 2 when deliveryDate given rush is true and ny', t => {
+    //given
+    let isRush = true;
+    let anOrder = {
+      deliveryState: 'NY', placedOn: {
+        plusDays: (x) => {
+          return x
+        }
+      }
+    }
+    //when
+    const result = deliveryDate(anOrder, isRush);
+    //then
+    t.is(result, 3);
+  });
