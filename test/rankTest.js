46a419ccab2b6443013372e049cb4f6fc1484644
vicky <jiajun_work@126.com>
2020-08-28 13:01:05 +0800

    [refactor homework][vicky] test: add test should return 2 when calculate captainHistoryRisk given voyage zone is china and history length = 6 and history not has china and 1 profit < 0 history.

--- test/rankTest.js
+++ test/rankTest.js
@@ -532,3 +532,38 @@ rankTest('should return 5 when calculate captainHistoryRisk given voyage zone is
   //then
   t.is(5, result)
 });
+rankTest('should return 2 when calculate captainHistoryRisk given voyage zone is china and history length = 6 and history not has china and 1 profit < 0 history', t => {
+  //given
+  const voyage = {
+    zone: 'china',
+    length: 10,
+  };
+  const history = [
+    {
+      zone: 'east-indies',
+      profit: 5,
+    }, {
+      zone: 'west-indies',
+      profit: 15,
+    }, {
+      zone: 'west-africa',
+      profit: -2,
+    },
+    {
+      zone: 'west-africa',
+      profit: 7,
+    },
+    {
+      zone: 'west-africa',
+      profit: 7,
+    },
+    {
+      zone: 'west-africa',
+      profit: 7,
+    },
+  ];
+  //when
+  const result = captainHistoryRisk(voyage,history);
+  //then
+  t.is(2, result)
+});
